b66557401a9ca33b007dd87dec0934545ff97e70
梦幻 <web2018_lang@163.com>
2020-03-02 18:59:11 +0800

    分类页静态布局

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,3 +1,5 @@
+import request from "../../utils/request.js"
+
 // pages/category/index.js
 Page({
 
@@ -5,62 +7,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    //记录当前点击了那个菜单
+    current:0,
+    //数据列表
+    list:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //请求分类页的数据
+    request({
+      //请求分类页的数据
+      url:"/categories"
+    }).then(res => {
+      //把数据雷暴爆粗到data
+      const {message} =res.data;
+      this.setData({
+        list:message
+      })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  
 })
